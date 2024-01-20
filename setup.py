5484ceacb4f270b78626ecf217b4a7ba2d3b48b2
kowshik24 <kowshikcseruet1998@gmail.com>
2024-01-21 05:10:00 +0600

    setput file updated

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
+import os
 from setuptools import setup, find_packages
+
+# Get the current directory where setup.py is located
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Open README.md with the correct path
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='PineconePDFExtractor',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     description='A library to process PDF files for Pineone',
     author='Kowshik Deb Nath',
     author_email='kowshikcseruet1998@gmail.com',
