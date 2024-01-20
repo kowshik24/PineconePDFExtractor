1e0ef424723f86163e9a419b7ba6a0d20e5a4809
kowshik24 <kowshikcseruet1998@gmail.com>
2024-01-21 01:09:18 +0600

    setup updated

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='PineconePDFExtractor',
     version='0.1',
     packages=find_packages(),
+    long_description=open('README.md').read(),
     description='A library to process PDF files for Pineone',
     author='Kowshik Deb Nath',
     author_email='kowshikcseruet1998@gmail.com',
